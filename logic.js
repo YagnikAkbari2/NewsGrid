8e7ca49e4d5eac073ab183a5c012f54410311a69
YagnikAkbari2 <yagnik_20003@ldrp.ac.in>
2023-07-08 09:00:20 +0530

    made some Changes

--- logic.js
+++ logic.js
@@ -32,6 +32,23 @@ const titleConverter = function (title) {
   return title;
 };
 
+const renderCard = () => {
+  let html = "";
+  for (let i = 0; i <= 6; i++) {
+    html += `<article class="empty-card">
+      <div class="empty-image strip"></div>
+      <div>
+        <div class="empty-category strip sloved"></div>
+        <div class="empty-title strip sloved"></div>
+        <p class="empty-description strip"></p>
+      </div>
+    </article>`;
+  }
+  document
+    .querySelector(".article-container")
+    .insertAdjacentHTML("afterBegin", html);
+};
+
 const renderData = function (data) {
   document.querySelector(".article-container").textContent = "";
   const html = data
@@ -73,7 +90,25 @@ const renderData = function (data) {
     .insertAdjacentHTML("beforeend", html);
 };
 
+const renderMainSection = (news) => {
+  const html = `
+  <div class="showcase-content">
+      <div class="catagory catagory-sports">SPORTS</div>
+      <h1>${news.title}</h1>
+      <p>
+        ${news.description}
+      </p>
+      <p>${news.publishDate}</p>
+      <a href="#home-articles" class="btn btn-primary">Read More</a>
+    </div>
+  `;
+  document
+    .querySelector(".showcase-container")
+    .insertAdjacentHTML("beforeend", html);
+};
+
 const getData = async function () {
+  renderCard();
   try {
     const response = await fetch(
       `${API_URL}search? keywords=Amazon&language=en&apiKey=${API_KEY}`
@@ -83,13 +118,33 @@ const getData = async function () {
     const data = await response.json();
 
     const newsData = data.news.map((obj) => createObject(obj));
-
-    renderData(newsData);
+    setTimeout(() => {
+      renderData(newsData);
+    }, 2000);
   } catch (err) {
     console.warn(`${err} @@@@@@@ ${err.message}`);
   }
 };
-const init = function () {
-  setTimeout(getData, 2000);
+
+const getMoreData = async () => {
+  const res = await fetch(
+    "https://api.marketaux.com/v1/news/all?symbols=TSLA%2CAMZN%2CMSFT&filter_entities=true&language=en&api_token=DhwX55w98hXvkYSsbLbNStzMWYACejXWaRN5HZ3k"
+  );
+  const data = await res.json();
+  console.log(data.data[0]);
+  const news = data.data[0];
+  const generateDate = new Date(news.published_at)
+    .toLocaleString()
+    .split(",")[0];
+  const generateNewsObject = {
+    title: news.title,
+    description: news.description,
+    publishDate: generateDate,
+    imageURL: news.image_url,
+    url: news.url,
+  };
+  renderMainSection(generateNewsObject);
 };
-init();
+
+getMoreData();
+getData();
